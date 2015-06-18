3cd2f0c76dd74a62d90b625cae63626b5b21d309
draculavlad <450416583@qq.com>
2015-06-18 14:42:12 +0800

    Update mesos-master-setup.sh

--- mesos-master-setup.sh
+++ mesos-master-setup.sh
@@ -37,7 +37,7 @@ echo "$master_node_ip" > "/etc/mesos-master/ip" && \
 echo "$master_node_ip" > "/etc/mesos-master/hostname" && \
 cp /etc/mesos/zk /etc/marathon/conf/master && \
 cp /etc/marathon/conf/master /etc/marathon/conf/zk && \
-sed –i 's|mesos|marathon|g' /etc/marathon/conf/zk && \
+sed -i 's|mesos|marathon|g' /etc/marathon/conf/zk && \
 cd /opt && wget "https://raw.githubusercontent.com/draculavlad/SetUpMesosphereOnCentos7WithServiceDiscovery/master/haproxy-marathon-bridge" && \
 echo "$marathon_node_ip:8080" >> /etc/haproxy-marathon-bridge/marathons && \
 chmod +x /opt/haproxy-marathon-bridge && \
