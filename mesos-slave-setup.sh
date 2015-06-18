29df757a88d3cb1c6d07891a7828c399968c3e33
draculavlad <450416583@qq.com>
2015-06-18 16:29:17 +0800

    Update mesos-slave-setup.sh

--- mesos-slave-setup.sh
+++ mesos-slave-setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 yum install -y net-tools && \
-export master_node_ip= && \
+export master_node_ip="192.168.212.163" && \
 export local_ip=$(ifconfig eno16777736 | grep 'inet ' | awk '{ print $2}') && \
-export slave_node_ip="$local_ip"
-
+export slave_node_ip="$local_ip" && \
+echo "local slave ip is: $slave_node_ip" && \
+echo "remote master ip is: $master_node_ip" && \
 rpm -Uvh http://repos.mesosphere.io/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm && \
 rpm -Uvh http://archive.cloudera.com/cdh4/one-click-install/redhat/6/x86_64/cloudera-cdh-4-0.x86_64.rpm && \
 setenforce Permissive && \
@@ -16,9 +17,11 @@ systemctl disable mesos-master && \
 systemctl disable mesos-slave && \
 systemctl disable marathon && \
 systemctl disable zookeeper && \
+echo "setup is ok, begin configuring" && \
 echo "zk://$master_node_ip:2181/mesos" > /etc/mesos/zk && \
 echo "$slave_node_ip" > "/etc/mesos-slave/ip" && \
 echo "$slave_node_ip" > "/etc/mesos-slave/hostname" && \
 echo 'docker,mesos' > /etc/mesos-slave/containerizers && \
 echo '5mins' > /etc/mesos-slave/executor_registration_timeout && \
-systemctl start mesos-slave
+systemctl start mesos-slave && \
+echo "done ..."
